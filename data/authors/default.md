2cdbd842683421a4d032d34fb468431c71cd7ca0
liangchaofei <chaofei_liang@126.com>
2023-03-23 22:17:06 +0800

    fix: author

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,19 +1,21 @@
 ---
-name: 狂奔滴小马
+name: 程序员库里
 avatar: /static/images/avatar.png
 occupation: 前端工程师
 company: 分享 JavaScript 热门框架，记录前端工程师学习成长历程。
-email: maqi1520@163.com
-juejin: https://juejin.cn/user/2189882895384093
+email: 825605688@qq.com
+juejin: https://juejin.cn/user/1046390799881655
 zhihu: https://www.zhihu.com/people/xiao-ma-15-3
-github: https://github.com/maqi1520
+github: https://github.com/liangchaofei
 ---
 
-- 姓名：马琪斌
-- 昵称：狂奔滴小马
+- 姓名：梁朝飞
+- 昵称：程序员库里
+- 抖音：程序员库里
+- B 站：程序员库里
 - 座右铭：你不一定要很厲害，才能開始；但你要開始，才能很厲害
 - 我是一名前端工程师，也是一名 Javascript 全栈开发者，长期关注前端工程化，开发体验优化等方向，近期对低代码、微前端、serverless、vite 有浓厚兴趣并实践中
-- 坐标：杭州
+- 坐标：北京
 - skill：
   - 前端：React、ES6/7/8、Antd、Axios、Tailwindcss
   - 服务端：Next.js、Koa2、Prisma、Typeorm
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: '狂奔滴小马 | 前端工程师成长部落阁',
-  author: '狂奔滴小马',
-  headerTitle: 'curry部落阁',
+  title: '程序员库里 | 前端工程师成长部落阁',
+  author: '程序员库里',
+  headerTitle: '朝飞的博客',
   description: '分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,9 +10,9 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'maqi1520@163.com',
-  github: 'https://github.com/maqi1520',
-  juejin: 'https://juejin.cn/user/2189882895384093',
+  email: '825605688@qq.com',
+  github: 'https://github.com/liangchaofei',
+  juejin: 'https://juejin.cn/user/1046390799881655',
   zhihu: 'https://www.zhihu.com/people/xiao-ma-15-3',
   locale: 'zh-CN',
   welcome: '自在，轻盈，我本不想停留',
